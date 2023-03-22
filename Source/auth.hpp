d32dbf98ec1bf9fb9795cb7e1ce9786024bf28f4
Serups <SarnaxOfficial@protonmail.com>
2023-03-23 05:25:47 +0700

    Organize a new code Not to be duplicate

--- Source/auth.hpp
+++ Source/auth.hpp
@@ -3,6 +3,10 @@
 #include <vector>
 #include <fstream>
 
+#include <optional>
+#include <string>
+#include <vector>
+
 namespace KeyAuth {
 
     struct ApiData {
@@ -22,9 +26,6 @@ namespace KeyAuth {
         std::string message;
     };
 
-#include <optional>
-#include <string>
-#include <vector>
 
 // Define the ApiData type
 struct ApiData {
@@ -33,88 +34,6 @@ struct ApiData {
   std::string secret;
   std::string version;
   std::string url;
-};
-
-class Api {
- public:
-  Api(const std::string& name,
-      const std::string& ownerId,
-      const std::string& secret,
-      const std::string& version,
-      const std::string& url);
-
-  void ban(std::optional<std::string> reason = std::nullopt) const;
-  void init() const;
-  void check() const;
-  void log(const std::string& message) const;
-  void license(const std::string& key) const;
-  std::string getVar(const std::string& varId) const;
-  std::string webhook(const std::string& id, const std::string& params) const;
-  void setVar(const std::string& varId, const std::string& varData) const;
-  bool checkBlacklist() const;
-  void upgrade(const std::string& username, const std::string& key) const;
-  void login(const std::string& username, const std::string& password) const;
-  void webLogin() const;
-  void submitButton(const std::string& buttonValue) const;
-  std::vector<unsigned char> downloadFile(const std::string& fileId) const;
-  void registerUser(const std::string& username,
-                    const std::string& password,
-                    const std::string& key) const;
-
-  const ApiData& getData() const;
-
- private:
-  std::string sessionId_;
-  std::string encryptionKey_;
-  ApiData data_;
-};
-
-
-class Api {
-public:
-  Api(std::ostream& logStream) : m_logStream(logStream) {}
-
-  void ban(const std::string& userId, const std::optional<std::string>& reason = std::nullopt) const {
-    if (reason) {
-      m_logStream << "User " << userId << " banned for reason: " << *reason << std::endl;
-    } else {
-      m_logStream << "User " << userId << " banned" << std::endl;
-    }
-  }
-
-  void init() const {
-    // TODO: Implement initialization code
-    m_logStream << "API initialized" << std::endl;
-  }
-
-  void check() const {
-    // TODO: Implement API health check
-    m_logStream << "API health check passed" << std::endl;
-  }
-
-  void log(const std::string& message) const {
-    m_logStream << message << std::endl;
-  }
-
-  bool verifyLicense(const std::string& key) const {
-    // TODO: Implement license verification
-    m_logStream << "License verification successful" << std::endl;
-    return true;
-  }
-
-  std::string getVar(const std::string& varId) const {
-    // TODO: Implement variable retrieval
-    return "";
-  }
-
-  bool sendWebhook(const std::string& url, const std::string& payload) const {
-    // TODO: Implement webhook sending
-    m_logStream << "Webhook sent to " << url << std::endl;
-    return true;
-  }
-
-private:
-  std::ostream& m_logStream;
 };
 
 
